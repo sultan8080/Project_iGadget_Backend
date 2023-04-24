bf7c5951501c07bbb5fb9b2a2200ffbf04df11dd
Marie Baude <33251784+MarieBaude@users.noreply.github.com>
2023-04-24 10:29:25 +0200

    adding orders entity

---
+++ src/Entity/Orders.php
@@ -0,0 +1,51 @@
+<?php
+
+namespace App\Entity;
+
+use App\Repository\OrdersRepository;
+use Doctrine\DBAL\Types\Types;
+use Doctrine\ORM\Mapping as ORM;
+
+#[ORM\Entity(repositoryClass: OrdersRepository::class)]
+class Orders
+{
+    #[ORM\Id]
+    #[ORM\GeneratedValue]
+    #[ORM\Column]
+    private ?int $id = null;
+
+    #[ORM\Column(type: Types::DATE_MUTABLE)]
+    private ?\DateTimeInterface $orderDate = null;
+
+    #[ORM\Column(type: Types::DATE_MUTABLE)]
+    private ?\DateTimeInterface $approvalDate = null;
+
+    public function getId(): ?int
+    {
+        return $this->id;
+    }
+
+    public function getOrderDate(): ?\DateTimeInterface
+    {
+        return $this->orderDate;
+    }
+
+    public function setOrderDate(\DateTimeInterface $orderDate): self
+    {
+        $this->orderDate = $orderDate;
+
+        return $this;
+    }
+
+    public function getApprovalDate(): ?\DateTimeInterface
+    {
+        return $this->approvalDate;
+    }
+
+    public function setApprovalDate(\DateTimeInterface $approvalDate): self
+    {
+        $this->approvalDate = $approvalDate;
+
+        return $this;
+    }
+}
