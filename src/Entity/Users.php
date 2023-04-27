b68ffc3e976d69121bf0bc9982b2a72be2c7f922
Moti-Sultan <motisultannur@gmail.com>
2023-04-27 13:29:27 +0200

    Email Vérification success :User registration

--- src/Entity/Users.php
+++ src/Entity/Users.php
@@ -13,6 +13,7 @@ use Symfony\Component\Security\Core\User\PasswordAuthenticatedUserInterface;
 
 #[ORM\Entity(repositoryClass: UsersRepository::class)]
 #[UniqueEntity(fields: ['email'], message: 'Il existe déjà un compte avec cet email')]
+#[UniqueEntity(fields: ['email'], message: 'There is already an account with this email')]
 class Users implements UserInterface, PasswordAuthenticatedUserInterface
 {
     #[ORM\Id]
