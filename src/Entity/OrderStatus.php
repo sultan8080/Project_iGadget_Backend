233fee59538730901629d5b4dff9ac1ddd74253d
Marie Baude <33251784+MarieBaude@users.noreply.github.com>
2023-05-03 10:24:23 +0200

    adding API Platform anotation in order status, orders and order returns

--- src/Entity/OrderStatus.php
+++ src/Entity/OrderStatus.php
@@ -6,8 +6,10 @@ use App\Repository\OrderStatusRepository;
 use Doctrine\Common\Collections\ArrayCollection;
 use Doctrine\Common\Collections\Collection;
 use Doctrine\ORM\Mapping as ORM;
+use ApiPlatform\Metadata\ApiResource;
 
 #[ORM\Entity(repositoryClass: OrderStatusRepository::class)]
+#[ApiResource]
 class OrderStatus
 {
     #[ORM\Id]
