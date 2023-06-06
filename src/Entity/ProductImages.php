98f31d3cb02e206246f2ebb982a3ae08af684886
Marie Baude <33251784+MarieBaude@users.noreply.github.com>
2023-06-06 09:28:26 +0200

    adding content for upload image

--- src/Entity/ProductImages.php
+++ src/Entity/ProductImages.php
@@ -5,9 +5,40 @@ namespace App\Entity;
 use App\Repository\ProductImagesRepository;
 use Doctrine\ORM\Mapping as ORM;
 use ApiPlatform\Metadata\ApiResource;
+use Vich\UploaderBundle\Mapping\Annotation as Vich;
 
+#[Vich\Uploadable]
 #[ORM\Entity(repositoryClass: ProductImagesRepository::class)]
-#[ApiResource]
+#[ApiResource(
+    normalizationContext: ['groups' => ['media_object:read']], 
+    types: ['https://schema.org/MediaObject'],
+    operations: [
+        new Get(),
+        new GetCollection(),
+        new Post(
+            controller: CreateMediaObjectAction::class, 
+            deserialize: false, 
+            validationContext: ['groups' => ['Default', 'media_object_create']], 
+            openapi: new Model\Operation(
+                requestBody: new Model\RequestBody(
+                    content: new \ArrayObject([
+                        'multipart/form-data' => [
+                            'schema' => [
+                                'type' => 'object', 
+                                'properties' => [
+                                    'file' => [
+                                        'type' => 'string', 
+                                        'format' => 'binary'
+                                    ]
+                                ]
+                            ]
+                        ]
+                    ])
+                )
+            )
+        )
+    ]
+)]
 class ProductImages
 {
     #[ORM\Id]
@@ -17,6 +48,18 @@ class ProductImages
 
     #[ORM\Column(length: 255)]
     private ?string $image_name = null;
+
+    #[ApiProperty(types: ['https://schema.org/contentUrl'])]
+    #[Groups(['media_object:read'])]
+    public ?string $contentUrl = null;
+
+    #[Vich\UploadableField(mapping: "media_object", fileNameProperty: "filePath")]
+    #[Assert\NotNull(groups: ['media_object_create'])]
+    public ?File $file = null;
+
+    #[ORM\Column(nullable: true)] 
+    public ?string $filePath = null;
+
 
     #[ORM\ManyToOne(inversedBy: 'productimages')]
     #[ORM\JoinColumn(nullable: false)]
