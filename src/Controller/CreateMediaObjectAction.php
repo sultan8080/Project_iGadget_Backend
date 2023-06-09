9bc4d796251edecfc917b916a63aa6442a6281d0
Marie Baude <33251784+MarieBaude@users.noreply.github.com>
2023-06-09 23:00:12 +0200

    delete photo and adding file upload in gitignore

--- src/Controller/CreateMediaObjectAction.php
+++ src/Controller/CreateMediaObjectAction.php
@@ -37,7 +37,10 @@ final class CreateMediaObjectAction extends AbstractController
         $mediaObject->setImageName($imageName);
         $mediaObject->setProducts($product);
 
-        // dd($mediaObject);
+        // Ajouter un message pour préciser le bon enregistrement d'un produit
+        $message = 'Le produit a été enregistré avec succès !';
+        $this->addFlash('success', $message);
+
 
         return $mediaObject;
     }
